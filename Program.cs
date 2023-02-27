2c3761395e8d2bf71d9038d88cec4cd7af609e5d
Krishanaku <krishnakumar.kk650.kk@gmail.com>
2023-02-27 15:34:02 +0530

    Add : Ability to create the binary tree shown in the figure

--- Program.cs
+++ Program.cs
@@ -11,9 +11,18 @@ namespace HashBinary
             b1.Add(56);
             b1.Add(30);
             b1.Add(70);
+            b1.Add(60);
+            b1.Add(95);
+            b1.Add(65);
+            b1.Add(63);
+            b1.Add(67);
+            b1.Add(22);
+            b1.Add(40);
+            b1.Add(11);
+            b1.Add(3);
+            b1.Add(16);
 
             b1.TraversePreorder(b1.root);
-
 
         }
     }
